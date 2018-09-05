fb7cb1ac1c4d80c562ca87f762dc4dded00fbce5
gwfWfkkan <1780363806@qq.com>
2018-09-06 01:16:34 +0800

    xiaomi3

--- ka.js
+++ ka.js
@@ -2,14 +2,14 @@
 * @Author: gf880623
 * @Date:   2018-09-04 21:47:32
 * @Last Modified by:   gf880623
-* @Last Modified time: 2018-09-04 21:55:49
+* @Last Modified time: 2018-09-06 00:43:35
 */
 	    function card(box,son){
 	    	for(let i=0;i<box.length;i++){
 			box[i].onmouseover=function(){
-			// for(let j=0;j<son.length;j++){
-			// 	son[j].style.display="none";
-			// }
+			for(let j=0;j<son.length;j++){
+				son[j].style.display="none";
+			}
 			son[i].style.display="block";
 			}
 			box[i].onmouseout=function(){
@@ -17,3 +17,42 @@
 			}
 		}
 	}
+
+
+function Dingbu( fh){
+	fh.onclick=function(){
+		animate(document.body,{scrollTop:0});
+		animate(document.documentElement,{scrollTop:0});
+		
+	}	
+}
+window.onscroll=function(){
+	let dongs=document.body.scrollTop||document.documentElement.scrollTop;
+	let fh=document.querySelectorAll(".bar-link")[3];
+	if(dongs>1000){
+		fh.style.display="block";
+	} else{
+		fh.style.display="none";
+	}
+}
+// window.onscroll=function(){
+// 	if(document.body.scrollTop>500){
+// 			fh.style.display="block";
+// 		}
+// 		else{
+// 			fh.style.display="none";
+// 		}
+// 	}
+// 	
+// 	
+
+function Tab(lis,card){
+	for(let i=0;i<lis.length;i++){
+		lis[i].onmouseover=function(){
+			for(let j=0;j<lis.length;j++){
+				card[j].style.display="none";
+			}
+			card[i].style.display="block";
+		}
+	}
+}
